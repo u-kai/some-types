db95457a7552ad81f008374f2298771e5f79ca85
u-kai <76635578+u-kai@users.noreply.github.com>
2021-07-08 06:37:32 +0900

    clear cellcolumnIndex type

---
+++ CellColumnIndex.ts
@@ -0,0 +1,8 @@
+declare const cellColumnIndexNominality:unique symbol
+
+export type CellColumnIndex = string & {[cellColumnIndexNominality]:never}
+//"A","AA","AB"
+
+export const isCellColumnIndex = (value:string):value is CellColumnIndex => {
+    return /[^A-Z]+/.test(value)    
+}
