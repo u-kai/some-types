c85cc5270928c2e3268903600d710b3be53c5d3d
u-kai <76635578+u-kai@users.noreply.github.com>
2021-07-08 06:41:45 +0900

    clear CellRowIndex type

---
+++ CellRowIndex.ts
@@ -0,0 +1,8 @@
+declare const cellRowIndexNominality:unique symbol
+
+export type CellRowIndex = string & {[cellRowIndexNominality]:never}
+//"3","27"
+
+export const isCellRowIndex = (value:string):value is CellRowIndex=>{
+    return /^[1-9]+/.test(value) 
+}
